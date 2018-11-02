899eb388de09caafd77ec9c756b478d1a07409c0
piszosta <piotrszostak@gmail.com>
2018-11-02 01:06:41 +0100

    testing with mentor

---
+++ kodilla- course/kodilla-testing/src/main/java/com/kodilla/testing/Calculator.java
@@ -0,0 +1,9 @@
+package com.kodilla.testing;
+
+public class Calculator {
+    public int adding (int a, int b){
+        return a + b;
+
+    }
+
+}
---
+++ kodilla- course/kodilla-testing/src/test/java/com/kodilla/testing/CalculatorTest.java
@@ -0,0 +1,21 @@
+package com.kodilla.testing;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    @Test
+    public void testAdding (){
+        Calculator calc = new Calculator ();
+
+        int result = calc.adding(1,3);
+
+        Assert.assertEquals(5,result);
+
+
+
+    }
+
+}
