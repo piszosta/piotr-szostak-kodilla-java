91a50fef22828e8195f7d5b4ceb2d947eb86c942
piszosta <piotrszostak@gmail.com>
2018-11-29 03:44:36 +0100

    6.2

---
+++ kodilla- course/kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,71 @@
+package com.kodilla.testing.collection;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class OddNumbersExterminator {
+    private static List<Integer> numbers;
+    private static List<Integer> even;
+    private static Scanner sc;
+
+    public OddNumbersExterminator() {
+        numbers = new ArrayList<>();
+        even = new ArrayList<>();
+        sc = new Scanner(System.in);
+    }
+
+        public void addEven (){
+            for (int n : numbers) {
+                if (n % 2 == 0) even.add(n);
+            }
+        }
+        public void fillNumbers () {
+            int n = sc.nextInt();
+            do {
+                numbers.add(n);
+                n = sc.nextInt();
+            } while (n != 0);
+        }
+        public void display (List < Integer > even) {
+            for (Integer i : even)
+                System.out.println(i);
+        }
+
+        public List<Integer> getEven () {
+            return even;
+        }
+        public static void main (String[]args) throws java.lang.Exception {
+
+            OddNumbersExterminator exterminator = new OddNumbersExterminator();
+            exterminator.fillNumbers();
+            exterminator.addEven();
+            System.out.println("even");
+        }
+
+}
+
+
+
+            /*public OddNumbersExterminator (int numbers){
+        this.numbers = numbers;
+
+public class Tester
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+    ArrayList<Integer> listOfNumbera = new ArrayList<Integer>();
+    Random theGenerator = new Random();
+
+		for (int n=0; n < 10; n++){
+        listOfNumbers.add(new OddNumbersExterminator(theGenerator.nextInt(30)));
+    }
+    public void exterminate(ArrayList<Integer> numbers)
+         if (numbers%2 == 0)
+
+    }*/
+
+
